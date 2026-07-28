6aa96f2dd81ddf64dc17d45c4373b9e48dbb1145
cktjgus <cktjgus04@naver.com>
2026-07-28 13:38:09 +0700

    feat: map api 연동

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = import.meta.env.DEV ? 'http://localhost:3005' : 'https://api.knu80th.shop'
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? 'https://api.knu80th.shop'
 
 
 
